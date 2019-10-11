768cdfda730c94d3d21dd3513620b35df2b6280f
Kat <kat.ochmanska@gmail.com>
2019-10-11 11:01:05 +0200

    student entity created

---
+++ src/main/java/com/kat/entity/Student.java
@@ -0,0 +1,13 @@
+package com.kat.entity;
+
+public class Student {
+
+    private int id;
+    private String name;
+    private String surname;
+    private String year;
+    private String courses;
+    private int tuitionBalance;
+    private int courseCost = 600;
+
+}
